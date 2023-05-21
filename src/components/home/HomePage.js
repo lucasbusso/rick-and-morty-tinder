d7fc1a9a0d5d523e721aa2cf197671101bc9af6d
Lucas Busso <lucas.busso.cft@jcrew.com>
2023-05-21 19:08:37 -0300

    Redux hydrate Home component using connect method

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,38 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import Card from '../card/Card'
 import styles from './home.module.css'
-import axios from 'axios'
+import { connect } from 'react-redux'
 
-let URL = "https://rickandmortyapi.com/api"
-
-export default function Home() {
-
-    let [chars, setChars] = useState([])
-
-    useEffect(() => {
-        getCharacters()
-    }, [])
-
-    function nextChar() {
-        chars.shift()
-        if (!chars.length) {
-            //get more characters
-        }
-        setChars([...chars])
-    }
+function Home({ chars }) {
 
     function renderCharacter() {
         let char = chars[0]
         return (
-            <Card leftClick={nextChar} {...char} />
+            <Card {...char}/>
         )
-    }
-
-    function getCharacters() {
-        return axios.get(`${URL}/character`)
-            .then(res => {
-                setChars(res.data.results)
-            })
     }
 
     return (
@@ -44,3 +21,12 @@ export default function Home() {
         </div>
     )
 }
+
+// Recibe el store y los mapea a las props del componente
+function mapStateToProps(state) {
+    return {
+        chars: state.characters.array
+    }
+}
+
+export default connect(mapStateToProps)(Home)
