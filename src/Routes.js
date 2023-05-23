98b6fb87715565be46031e579a38f5a38a307045
Lucas Busso <lucas.busso.cft@jcrew.com>
2023-05-23 19:45:55 -0300

    Component working with graphql finished but Route switch to rest api

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 import Home from './components/home/HomePage'
 import FavPage from './components/favs/FavPage'
 import LoginPage from './components/login/LoginPage'
-import GraphHome from './components/home/GraphHome'
+// import GraphHome from './components/home/GraphHome'
 
 function PrivateRout({ path, component, ...rest }) {
     let storage = localStorage.getItem("storage")
@@ -19,7 +19,7 @@ function PrivateRout({ path, component, ...rest }) {
 export default function Routes() {
     return (
         <Switch>
-            <PrivateRout exact path="/" component={GraphHome} />
+            <PrivateRout exact path="/" component={Home} />
             <PrivateRout path="/favs" component={FavPage} />
             <Route path="/login" component={LoginPage} />
         </Switch>
--- src/components/home/GraphHome.js
+++ src/components/home/GraphHome.js
@@ -4,25 +4,42 @@ import { gql } from 'apollo-boost'
 import { useQuery } from 'react-apollo'
 
 export default function GraphHome() {
+    let [chars, setChars] = useState([])
+    
+    
     let query = gql`
-        {
-            characters {
-                results {
-                    name
-                    image
-                }
+    {
+        characters {
+            results {
+                name
+                image
             }
         }
+    }
     `
     let {data, loading, error} = useQuery(query)
-    console.log(data)
     
+    useEffect(()=>{
+        if(data && !loading && !error) {
+            setChars([...data.characters.results])
+        }
+    }, [data])
+
+    function nextCharacter() {
+        chars.shift()
+        setChars([...chars])
+    }
+
+    function addToFavorites() {
+        chars.
+    }
+        
     if(loading) return <h2>Cargando personajes</h2>
     return (
         <Card 
-            // leftClick={nextCharacter} 
-            // rightClick={addToFavorites}
-            {...data.characters.results[0]}
+            leftClick={nextCharacter} 
+            rightClick={addToFavorites}
+            {...chars[0]}
         />
     )
 }
