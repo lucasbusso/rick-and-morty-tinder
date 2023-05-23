9c31d376cc91bede85e8cc55a330e85699a60852
Lucas Busso <lucas.busso.cft@jcrew.com>
2023-05-23 00:06:00 -0300

    apollo provider app wrapper to consume API with graphql

--- src/index.js
+++ src/index.js
@@ -7,13 +7,19 @@ import { BrowserRouter } from 'react-router-dom'
 import 'font-awesome/css/font-awesome.css'
 import { Provider } from 'react-redux'
 import generateStore from './redux/store'
+import { ApolloClient } from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 
 let store = generateStore()
+let client = new ApolloClient({
+    uri: "https://rickandmortyapi.com/graphql"
+})
 
 let WithRouter = () => <BrowserRouter><App /></BrowserRouter>
 let WithRedux = () => <Provider store={store}><WithRouter/></Provider>
+let WithApollo = () => <ApolloProvider client={client}><WithRedux/></ApolloProvider>
 
-ReactDOM.render(<WithRedux />, document.getElementById('root'));
+ReactDOM.render(<WithApollo />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
